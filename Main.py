1d85b0b09ad342e23dea1970d765444ec8c2bd76
Ryanbran888 <ryanbrandtstem@gmail.com>
2025-04-05 13:16:41 -0400

    meow meow added abilities meow meow

--- Main.py
+++ Main.py
@@ -9,6 +9,8 @@ running = True
 hearts = pygame.image.load(os.path.join('Artwork/Heart.png'))
 current = pygame.image.load(os.path.join('Artwork/DRAW CARD ANIMATION.png'))
 draw = []
+gold = 0
+totalGold = 0
 heartCount = 3
 blankCard = pygame.image.load(os.path.join('Artwork/DRAW CARD ANIMATION.PNG'))
 class Button(object):
@@ -30,14 +32,85 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN: 
             if nextButton.check_press(event.pos):
                 rnd = random.randint(1,25)
-                current = pygame.image.load(os.path.join(f'Artwork/card{rnd}.png')) 
-            #if(nextButton.check_press(event.pos)):
+                current = pygame.image.load(os.path.join(f'Artwork/card{rnd}.png'))
+                if(rnd == 1):
+                    heartCount = 0
+                elif(rnd == 2):
+                    gold += 5
+                elif(rnd == 3):
+                    gold += 10
+                elif(rnd == 4):
+                    # peak
+                    rnd = 5
+                elif(rnd == 5):
+                    heartCount -= 1
+                elif(rnd == 6):
+                    heartCount += 1
+                elif(rnd == 7):
+                    gold = gold * 2
+                elif(rnd == 8):
+                    heartCount -= 1
+                    if(gold % 2 == 0):
+                       gold = gold/2
+                    else:
+                        gold = gold/2
+                        difference = 1 - gold
+                        gold += (difference*-1)
+                elif(rnd == 9):
+                    rnd = 10
+                    #shuffle
+                elif(rnd == 10):
+                    gold += 1
+                elif(rnd == 11):
+                    gold = gold * 2
+                elif(rnd == 12):
+                    gold = gold *0
+                elif(rnd == 13):
+                    #multiuple choice
+                    rnd = 14
+                elif(rnd == 14):
+                    #ahgain more choice
+                    rnd = 15
+                elif(rnd == 15):
+                    heartCount+= 2
+                elif(rnd == 16):
+                    gold+= 15
+                elif(rnd == 17):
+                    gold+= 10
+                elif(rnd == 18):
+                    gold+= 10
+                elif(rnd == 19):
+                    gold+= 9
+                elif(rnd == 20):
+                    gold+= 8
+                elif(rnd == 21):
+                    gold+= 7
+                elif(rnd == 22):
+                    gold+= 6
+                elif(rnd == 23):
+                    gold+= 5
+                elif(rnd == 24):
+                    gold+= 4
+                elif(rnd == 25):
+                    gold+= 3
+            #if(endButton.check_press(event.pos)):
             
     screen.blit(blankCard, (950, 200))            
     nextButton.draw(screen)
     endButton.draw(screen)
     screen.blit(current, (450,375))
-    if(heartCount == 3):
+    if(heartCount == 5):
+        screen.blit(hearts, (0, 0))
+        screen.blit(hearts, (50, 0))
+        screen.blit(hearts, (100, 0))
+        screen.blit(hearts, (150, 0))
+        screen.blit(hearts, (200, 0))
+    elif(heartCount == 4):
+        screen.blit(hearts, (0, 0))
+        screen.blit(hearts, (50, 0))
+        screen.blit(hearts, (100, 0))
+        screen.blit(hearts, (150, 0))
+    elif(heartCount == 3):
         screen.blit(hearts, (0, 0))
         screen.blit(hearts, (50, 0))
         screen.blit(hearts, (100, 0))
