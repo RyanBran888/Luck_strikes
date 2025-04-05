9306f63d974980c5b02c3932882dc158dbd16fe8
Ryanbran888 <ryanbrandtstem@gmail.com>
2025-04-05 12:23:24 -0400

    meow

--- Main.py
+++ Main.py
@@ -9,19 +9,44 @@ running = True
 hearts = pygame.image.load(os.path.join('Artwork/Heart.png'))
 current = pygame.image.load(os.path.join('Artwork/card1.png'))
 draw = []
+heartCount = 3
+
+class Button(object):
+    def init(self, position, filename):
+        self.image = pygame.image.load(os.path.join(f'Artwork/{filename}'))
+        self.rect = self.image.get_rect(bottomright=position)
+    def draw(self,surface):
+        surface.draw(self.image,self.rect)
+    def check_press(self,position):
+        if(self.rect.collidepoint(*position)):
+            return True
+        return False
+nextButton = Button((100,100), "Drawcard.png")
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RETURN: 
-                draw.append(current)
-             
-    for img in draw:
-        screen.blit(img, (450,375))
-    screen.blit(hearts, (0, 0))
-    screen.blit(hearts, (50, 0))
-    screen.blit(hearts, (100, 0))
+                rnd = random.randint(1,25)
+                current = pygame.image.load(os.path.join(f'Artwork/card{rnd}.png')) 
+        if event.type == pygame.MOUSEBUTTONDOWN: 
+            if nextButton.check_press(event.pos):
+                print("meow meow")
+                
+    nextButton.draw(screen)
+    screen.blit(current, (450,375))
+    if(heartCount == 3):
+        screen.blit(hearts, (0, 0))
+        screen.blit(hearts, (50, 0))
+        screen.blit(hearts, (100, 0))
+    elif(heartCount == 2):
+        screen.blit(hearts, (0, 0))
+        screen.blit(hearts, (50, 0))
+    elif(heartCount == 1):
+        screen.blit(hearts, (0, 0))
+    #else:
+        #add round end
     pygame.display.flip()
     clock.tick(60)
     
